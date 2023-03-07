18b4507ef54fde74277532801f8c2772255acd08
Rajvardhan Jadhav <82108762+raj8888@users.noreply.github.com>
2023-03-08 01:09:37 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,1 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-// const connection = mongoose.connect(process.env.mongoURL)
-const connectDatabase = async () => {
-    try {      
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log('Connected to mongodb database successfully!')
-    } catch (err) {
-        console.log(err)
-    }
-}
 
-
-module.exports = { connectDatabase() };
