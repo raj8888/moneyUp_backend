fa7be834be6541243656254b30ecbc88e22505a6
Rajvardhan Jadhav <82108762+raj8888@users.noreply.github.com>
2023-03-07 15:24:25 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.set('bufferCommands', false);
 const connection = mongoose.connect(process.env.mongoURL)
-.then(()=>console.log("connected"))
-.catch(()=>{console.log(error)})
+
 
 module.exports = { connection };
