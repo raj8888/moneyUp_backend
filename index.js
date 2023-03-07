1095ff21e96b9f3e0f1052e49b3b93abe53ebbb7
Rajvardhan Jadhav <82108762+raj8888@users.noreply.github.com>
2023-03-07 10:03:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ const{HomeRouter}=require("./routes/Home.router");
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 app.use(cors());
 app.use("/user", UserRouter);
 
